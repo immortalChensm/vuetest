3c254afee770001fdf0b336c51afa3d8da9fec79
immortalChensm <1655664358@qq.com>
2020-02-12 16:19:02 +0800

    webpack-demo

---
+++ webpack-demo/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="container">
+        <p>hi,china</p>
+    </div>
+</template>
+
+<style>
+    body{
+        background:red;
+    }
+</style>
+
+<script>
+    console.log(111);
+</script>
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports={
+    entry:'./main.js',
+    output:{
+        path:__dirname,
+        filename:'build.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader:'vue-loader'
+            },
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                exclude:/node-modules/
+            }
+        ]
+    }
+}

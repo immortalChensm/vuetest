167a5a0f71626f4b90e662ec57f43368f7fef62e
immortalChensm <1655664358@qq.com>
2020-03-05 20:20:23 +0800

    test(es6):测试类
    类成员
    静态成员，可动态添加
    实例成员
    
    类继承extends
    父类可以调用子类任何成员
    子类也可以调用父类
    
    子类调用父类成员可以用super.成员属性或是方法
    子类构造调用父类构建super() 可以给参数

---
+++ ES6/class3.js
@@ -0,0 +1,58 @@
+
+class Person{
+
+    constructor(name){
+        console.log("person class"+name);
+    }
+    speaking(){
+        console.log("speaking\n");
+    }
+
+    doing(){
+        console.log("doing\n");
+    }
+
+    t(){
+        this.test();
+    }
+
+}
+Person.num=100;
+Person['kk']=()=>{
+    console.log("kk");
+}
+class Teacher extends Person
+{
+    constructor(name){
+        console.log("teacher class\n");
+        //console.log(this);
+        super(name);
+        console.log(this);
+
+        super.speaking();
+    }
+    gg(){
+       // this.getInfo();
+        this.doing();
+    }
+
+    test(){
+        console.log("teacher test\n");
+    }
+}
+
+$teacher = new Teacher("huahua");
+//$teacher.t();
+//$teacher.gg();
+// $teacher.speaking();
+// $teacher['name'] = "jack";
+// $teacher.age=100;
+// $teacher['getInfo']=()=>{
+//     return "hi,i am english teacher\n";
+// };
+
+
+//console.log($teacher.age);
+// console.log($teacher.num);
+// console.log(Teacher.num);
+// console.log($teacher.gg());
